5b983db1ca0648a7d27207acbdeefe66414b280c
saxonmatt <saxonmatt@gmail.com>
2013-10-25 15:41:36 +0100

    Copy document generation and associated styles/scripts to 'gh-pages' dir.

--- doc.cmd
+++ doc.cmd
@@ -7,3 +7,10 @@ echo assumes f# interactive is on your path...
 
 echo fsi Doc.fsx...
 fsi.exe Doc.fsx
+
+echo copy documentation to 'gh-pages' subfolder...
+xcopy "content" "gh-pages\content\" /s /y /u
+xcopy "ShrinkRay.html" "gh-pages\index.html" /y
+
+echo documentation complete!
+echo view ./index.html to review :)
