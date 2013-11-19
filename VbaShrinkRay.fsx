08fb67694a49889dde82646a1e62066b97afe0e0
saxonmatt <saxonmatt@gmail.com>
2013-11-19 12:21:54 +0000

    Dependecies and dependants now being mapped out, an a sample use case added.

--- VbaShrinkRay.fsx
+++ VbaShrinkRay.fsx
@@ -19,8 +19,8 @@ type CodeBlock = {
 
 let fqn codeblock = 
     match codeblock.Definition with
-    | Some (Definition def) -> sprintf "%s.%s" codeblock.SourceFile def
-    | None -> sprintf "%s.Global" codeblock.SourceFile
+    | Some (Definition def) -> Some(sprintf "%s.%s" codeblock.SourceFile def)
+    | None -> None
     
 let lowerCase (s:string) = s.ToLower()
 
@@ -66,7 +66,36 @@ let intoCodeBlocks filename (codeDom:CodeBlock list) line =
     | head :: tail -> 
         match line with
         | BodyLine words -> addLineOfCode head (LineOfCode words) :: tail
-        | DeclarationLine name -> codeBlock filename line :: codeDom
+        | DeclarationLine name -> codeBlock filename line :: codeDom        
+
+let hasDependency (Definition def) dependant = 
+    let exists = 
+        dependant.Lines
+        |> Seq.map (fun (LineOfCode line) -> line |> Seq.exists ((=)def))
+        |> Seq.exists ((=)true)
+    if exists then Some dependant else None
+
+let getDefinitions codeblocks = 
+    codeblocks 
+    |> List.choose (fun block -> 
+        match block.Definition with
+        | Some def -> Some block
+        | None -> None)
+    |> Seq.distinct 
+    |> Seq.sort 
+    |> List.ofSeq
+
+let getDependents codeblocks =
+    let findUsages dependency = 
+        match dependency.Definition with
+        | Some def -> 
+            let dependants = codeblocks |> Seq.choose (hasDependency def)            
+            dependency, (if Seq.length dependants > 0 then Some dependants else None)
+        | None -> 
+            dependency, None 
+    getDefinitions codeblocks 
+    |> Seq.map findUsages
+    |> Map.ofSeq
 
 let parse filename =     
     let parseAllLines =
@@ -78,3 +107,38 @@ let parse filename =
         >> Seq.map classifyCode
         >> Seq.fold (intoCodeBlocks filename) []    
     filename |> (File.ReadAllLines >> parseAllLines)
+
+let parseFiles filenames = 
+    filenames
+    |> Array.ofSeq 
+    |> Array.concat
+    |> Array.Parallel.map parse
+    |> Array.fold (@) []
+
+
+// ----------------------------------------------------------------------------
+// EXAMPLE
+// ----------------------------------------------------------------------------
+
+let sourceDirs = seq { 
+    yield Directory.GetFiles @"access_db_source\forms\"
+    yield Directory.GetFiles @"access_db_source\general\"
+    yield Directory.GetFiles @"access_db_source\modules\"
+    yield Directory.GetFiles @"access_db_source\queries\"
+    yield Directory.GetFiles @"access_db_source\reports\"
+    yield Directory.GetFiles @"access_db_source\scripts\"
+    yield Directory.GetFiles @"access_db_source\tables\" }
+
+sourceDirs 
+|> parseFiles 
+|> getDependents 
+|> Map.iter (fun dependency maybeDependants ->
+    let dependencyFqn = fqn dependency
+    match dependencyFqn, maybeDependants with
+    | Some fqn, Some dependants ->
+        let count = dependants |> Seq.length
+        printfn "'%s' has %i dependants, which are:" fqn count
+    | Some fqn, None -> 
+        printfn "'%s' has 0 dependants." fqn
+    | None, _ -> 
+        printfn "Incorrectly reported dependency in '%s'" dependency.SourceFile)
